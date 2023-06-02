399838a35783125e1fafffb7408ff545825f42a3
cjdemille <cjdemille5@gmail.com>
2023-06-02 14:20:43 -0400

    add etch functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 const main = document.querySelector('main');
 
+function changeColor(e){
+    e.target.style.backgroundColor = "black";
+}
 
 const makeDivs = (numDivs) =>{
     for (let i = 0; i < numDivs; i++){
         const square = document.createElement('DIV'); 
         square.classList.add('square');
+        square.addEventListener('mouseover', changeColor);
         main.append(square);
     }
 }
 
 makeDivs(256);
+
+
 
