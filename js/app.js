b03a59b293522ae590108fe88f72199bee237300
cjdemille <cjdemille5@gmail.com>
2023-06-02 15:26:09 -0400

    add ability to change grid density

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 const main = document.querySelector('main');
+const changeGridSize = document.querySelector('#change-squares')
 
 function changeColor(e){
     e.target.style.backgroundColor = "black";
 }
 
-const makeDivs = (numDivs) =>{
+const makeBoxes = (boxesPerSide) =>{
+    numDivs = boxesPerSide * boxesPerSide
     for (let i = 0; i < numDivs; i++){
+        main.style.gridTemplateRows = `repeat(${boxesPerSide}, 1fr)`;
+        main.style.gridTemplateColumns = `repeat(${boxesPerSide}, 1fr)`;
         const square = document.createElement('DIV'); 
         square.classList.add('square');
         square.addEventListener('mouseover', changeColor);
@@ -13,7 +17,23 @@ const makeDivs = (numDivs) =>{
     }
 }
 
-makeDivs(256);
+const removeBoxes = () =>{
+    main.innerHTML = "";
+}
 
+makeBoxes(16);
+
+changeGridSize.addEventListener('click', function(e){
+    let size = prompt('How many squares per side?');
+    size = Number(size);
+
+    if(size < 100){
+        removeBoxes();
+        makeBoxes(size);
+    } else{
+        alert("You need to pick a number less than 100");
+    };
+
+});
 
 
